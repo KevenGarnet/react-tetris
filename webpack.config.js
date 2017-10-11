eed9d28d10db8156ddc794ded82b9234b2ddb63c
刘龙洁 <liulongjie@go-goal.com>
2017-10-11 18:04:20 +0800

    install package step1

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+var webpack = require('webpack');
+// var precss = require('precss');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');//打开浏览器
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
